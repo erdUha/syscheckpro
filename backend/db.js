5f5b9f4b5c4d0218af09697f8e45c6824b9d3644
erdUha <theerden.kenzhebaev.2006@gmail.com>
2023-12-12 12:48:16 +0600

    Connection to local database

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+// Объявление переменных из окружения (Environment variables)
+const {
+	POSTGRES_HOST, // Хост базы данных. Обычно localhost
+	POSTGRES_DB_OS, // Название базы данных
+	POSTGRES_USER, // Пользователь с полномочиями управления базами данных
+	POSTGRES_PASSWORD // Пароль для пользователя
+} = process.env;
+
+// Создание нового пула
+const pool = new Pool({ 
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  host: POSTGRES_HOST,
+  port: 5432, // Стандартный порт для Postgres
+  database: POSTGRES_DB_OS
+});
+
+// Экспорт созданного пула
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
