3085078f83a62b6ccb6b272da3e6cff2bf8a41dc
erdUha <theerden.kenzhebaev.2006@gmail.com>
2023-12-12 12:50:33 +0600

    Main Vue app

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+</script>
+
+<template>
+	<NavBar />
+	<div id="router-wrapper">
+		<router-view id="router" v-slot="{ Component, route }">
+			<transition :name="route.meta.transition || 'slide-right'">
+				<component :is="Component" />
+			</transition>
+		</router-view>
+	</div>
+</template>
+
+<style src="./transitions.css"></style>
+
+<style>
+html, body {
+	padding: 0; margin: 0;
+}
+#page-two {
+	background-color: yellow;
+}
+#page-one {
+	background-color: cyan;
+}
+.pages {
+	padding: 50px;
+	width: 100%;
+	height: 100vh;
+}
+#router {
+	grid-row-start: 1;
+	grid-column-start: 1;
+}
+#router-wrapper {
+	display: grid; /* Для того, чтобы 2 странички с id="router" накладывались друг на друга */
+	grid-template-columns: 1fr;
+}
+#app {
+	position: relative;
+	overflow: hidden;
+	width: 100%; 
+}
+</style>
+
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+		NavBar,
+  }
+}
+</script>
