93c0e5d08f66aa820f989bfb94807047e018b342
erdUha <theerden.kenzhebaev.2006@gmail.com>
2023-12-13 12:40:22 +0600

    add

---
+++ src/components/Pages/PageOne.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+	<div>
+		<div class="pages" id="page-one">
+			<div data-aos="zoom-out-down">
+				<h1>Page One</h1>
+			</div>
+		</div>
+	</div>
+</template>
+
+
+<script>
+export default {
+	name: "PageOne",
+}
+</script>
